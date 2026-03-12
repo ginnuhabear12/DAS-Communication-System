3e3f99a235e6700c560c4474fb4e8aa32bb75a5c
ginnuhabear12 <ginnahebert@gmail.com>
2026-03-12 12:49:24 -0500

    Completed test GUI display (not interactable)

--- device/testing/GUItest/main.py
+++ device/testing/GUItest/main.py
@@ -1,55 +1,45 @@
-# from fastapi import FastAPI, Request
-# from fastapi.responses import HTMLResponse
-# from fastapi.staticfiles import StaticFiles
-# from fastapi.templating import Jinja2Templates
-# from device.testing.GUItest.services.modem_service import get_modem_signal
 
-# app = FastAPI()
-
-# app.mount("/static", StaticFiles(directory="static"), name="static")
-# templates = Jinja2Templates(directory="templates")
-
-
-# @app.get("/", response_class=HTMLResponse)
-# def home(request: Request):
-#     return templates.TemplateResponse("index.html", {"request": request})
-
-
-# @app.get("/settings", response_class=HTMLResponse)
-# def settings_page(request: Request):
-#     return templates.TemplateResponse("settings.html", {"request": request})
-
-
-# @app.get("/api/signal")
-# def get_signal():
-#     return get_modem_signal()
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from pathlib import Path
 
 app = FastAPI()
-
 BASE_DIR = Path(__file__).resolve().parent
 
-app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="static")
+app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 @app.get("/", response_class=HTMLResponse)
-def home():
-    return """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>DAS Test</title>
-        <link rel="stylesheet" href="/static/css/style.css">
-    </head>
-    <body>
+def dashboard(request: Request):
+    data = {
+        "site_name": "Hospital East Wing",
+        "device_id": "DAS-EMD-001",
+        "last_update": "2026-03-12 19:10:00",
+        "device_status": "ONLINE",
+        "modem_status": "CONNECTED",
+        "vpn_status": "ACTIVE",
+        "snmp_status": "RUNNING",
+        "rssi": "-70 dBm",
+        "rsrp": "-95 dBm",
+        "rsrq": "-10 dB",
+        "sinr": "18 dB",
+        "pci": "145",
+        "band": "n41",
+        "earfcn": "5230",
+        "cell_id": "0x31A4",
+        "alert_message": "No active alarms"
+    }
 
-        <h1>DAS Dashboard</h1>
-        <p id="msg">Loading...</p>
+    logs = [
+        {"time": "19:01", "rsrp": "-94 dBm", "sinr": "19 dB", "status": "Normal"},
+        {"time": "19:02", "rsrp": "-95 dBm", "sinr": "18 dB", "status": "Normal"},
+        {"time": "19:03", "rsrp": "-101 dBm", "sinr": "12 dB", "status": "Warning"},
+    ]
 
-        <script src="/static/js/app.js"></script>
+    return templates.TemplateResponse(
+        "dashboard.html",
+        {"request": request, "data": data, "logs": logs}
+    )
 
-    </body>
-    </html>
-    """
--- device/testing/GUItest/services/modem_service.py
+++
@@ -1,8 +0,0 @@
-def get_modem_signal():
-    # later this will run AT commands
-    return {
-        "band": "n78",
-        "RSRP": -92,
-        "RSRQ": -11,
-        "SINR": 20
-    }
